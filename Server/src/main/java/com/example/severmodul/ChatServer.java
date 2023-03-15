957cb569ae365a0f18f51e4490d5504071a71c8c
mblatter <mblatter@htl-steyr.ac.at>
2023-03-15 13:43:59 +0100

    initial commit

---
+++ Server/src/main/java/com/example/severmodul/ChatServer.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            /*String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }*/
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Thread sender = new Thread(new Runnable() {
+        String msg;
+        Scanner input = new Scanner(System.in);
+        @Override
+        public void run() {
+            while(true){
+                msg = input.nextLine();
+                out.println(msg);
+                out.flush();
+            }
+        }
+    });
+
+    Thread receiver = new Thread(new Runnable() {
+        String msg;
+        @Override
+        public void run() {
+            try {
+                msg = in.readLine();
+                while(msg != null){
+                    System.out.println("Client: "+msg);
+                    msg = in.readLine();
+                }
+                System.out.println("Client disconnected");
+                out.close();
+                clientSocket.close();
+                serverSocket.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    });
+
+    public static void main(String[] args) {
+        ChatServer server = new ChatServer();
+        server.start(6666);
+        server.sender.start();
+        server.receiver.start();
+    }
+}
