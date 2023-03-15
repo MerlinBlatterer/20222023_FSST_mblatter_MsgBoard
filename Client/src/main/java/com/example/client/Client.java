1b32e6ab96d210248d8093da2a3d7357612adc3f
matthiasbrenn <122985645+matthiasbrenn@users.noreply.github.com>
2023-03-15 08:31:19 +0100

    Commit Client Anwendung

---
+++ Client/src/main/java/com/example/client/Client.java
@@ -0,0 +1,25 @@
+package com.example.client;
+
+import java.util.Scanner;
+
+public class Client {
+
+    private static final String USERNAME = "user123";
+    private static final String PASSWORD = "pass123";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Benutzername: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Passwort: ");
+        String password = scanner.nextLine();
+
+        if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+            System.out.println("Login erfolgreich!");
+        } else {
+            System.out.println("Benutzername oder Passwort falsch!");
+        }
+    }
+}
