37472352f174aacda4bd1f2894a4fc2729475d1c
matthiasbrenn <122985645+matthiasbrenn@users.noreply.github.com>
2023-03-28 13:02:26 +0200

    Commit Client subscribed news

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 350);
         HelloController controller = fxmlLoader.getController();
         controller.start();
         primaryStage.setScene(scene);
